385035d05ef2b2274eeb5c059c2c772b6a49251f
chewyx212 <chewingyx212@gmail.com>
2021-07-05 16:36:24 +0800

    adjust

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-a=kh1s6mcqk4wi61s^#e_5b6db5a6a&q()th3u4&zr3y708)#$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
-ALLOWED_HOSTS = ['ec2-3-128-198-64.us-east-2.compute.amazonaws.com','127.0.0.1']
+ALLOWED_HOSTS = [
+    'ec2-18-222-182-168.us-east-2.compute.amazonaws.com', '127.0.0.1']
 
 
 # Application definition
